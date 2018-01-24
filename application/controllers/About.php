9dbf136ec0e7fe15ce6ab425cc77a5c0c14b2d86
Patrick Her <zivhsiao@gmail.com>
2018-01-24 17:13:43 +0800

    修改了一部分 ,about 比較有 model 連結 http://localhost:8888 http://localhost:8888/about

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,20 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class About extends CI_Controller {
 
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('about_model');
 	}
 	
 	public function index()
 	{
+        $news = $this->about_model->get_content();
 
 		$data = array(
-			'title' => "Title",
+            'news' => $news
 			);
 			
-		$this->twig->display('home', $data);	
+		$this->twig->display('about', $data);
 	}
 }
